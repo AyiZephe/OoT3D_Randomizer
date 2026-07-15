5c26cc028f1ecbb1a57bc9d25f5b672617fb2de8
AyiZephe <jacovie57@gmail.com>
2026-07-15 11:15:24 -0500

    Update pot.h

--- code/src/actors/pot.h
+++ code/src/actors/pot.h
@@ -1,1 +1,12 @@
+#ifndef _POT_H_
+#define _POT_H_
 
+#include "z3D/z3D.h"
+#include "z3D/actors/z_obj_tsubo.h"
+
+
+
+void OjbTsubo_rInit(Actor* thisx, GlobalContext* globalCtx);
+
+
+#endif //_CHEST_H_
