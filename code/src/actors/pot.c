1573eedcaedab907f13b6dba5427d617ac8f7b41
AyiZephe <jacovie57@gmail.com>
2026-07-15 11:28:39 -0500

    Create pot.c

---
+++ code/src/actors/pot.c
@@ -0,0 +1,11 @@
+#include "pot.h"
+#include "z3D/z3D.h"
+
+Void ObjTsubo_Init(Actor* thisx, GlobalContext* globalctx);
+
+Void ObjTsubo_rInit(Actor* thisx, GlobalContext* globalctx){
+     ObjTsubo_Init(thisx, globalctx);
+
+    thisx.params = 0x14;
+
+}
